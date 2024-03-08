e797533036b5ebc6067ef9ad6ec018c3dea34579
nguyentranlinhdat <datlinhnt.work@gmail.com>
2024-03-08 11:10:23 +0700

    done

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { quotes } from "../assets";
 
-const FeedbackCard = () => {
-  return <div>FeedbackCard</div>;
+const FeedbackCard = ({ content, name, title, img }) => {
+  return (
+    <div className="flex flex-col justify-between px-10 py-12 feedback-card rounded-[20px] my-5 mr-0 sm:mr-5 md:mr-10 max-w-[370px]">
+      <img
+        src={quotes}
+        alt="double_quotes"
+        className="w-[42px] h-[27px] object-contain"
+      />
+      <p className="my-10 font-normal text-white font-poppins leading-[32px] text-18px">
+        {content}
+      </p>
+
+      <div className="flex flex-row">
+        <img src={img} alt={name} className="w-[48px] h-[48px] rounded-full" />
+
+        <div className="flex flex-col ml-4">
+          <h4 className="font-semibold font-poppins text-[20px] leading-[32px] text-white">
+            {name}
+          </h4>
+          <p className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite">
+            {title}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FeedbackCard;
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,33 @@
 import React from "react";
-
+import styles from "../styles";
+import { feedback } from "../constants";
+import FeedbackCard from "./FeedbackCard";
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <section
+      className={`${styles.flexCenter} ${styles.paddingY} flex-col relative`}
+    >
+      {/*To do*/}
+      <div className="absolute w-[60%] h-[60%] z-[0] -right-[50%] rounded-full blue__gradient"></div>
+
+      <div className="flex flex-col items-center justify-between w-full mb-6 md:flex-row sm:mb-16 z-[1] relative">
+        <h1 className={styles.heading2}>
+          What people are <br className="hidden sm:block" /> saying about us
+        </h1>
+        <div className="w-full mt-6 md:mt-0">
+          <p className={`${styles.paragraph} max-w-[470px] text-left`}>
+            Everything you need to accept card payments and grow your business
+            anywhere on the planet.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+        {feedback.map((card) => (
+          <FeedbackCard key={card.id} {...card}></FeedbackCard>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
